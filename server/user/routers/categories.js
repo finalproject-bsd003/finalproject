bc61a21170bbc62f9dac3dde3e1a9b157720bd24
nickodsss <nickolasdarselio@gmail.com>
2023-07-27 14:25:10 +0700

    Adding category backend

--- server/user/routers/categories.js
+++ server/user/routers/categories.js
@@ -1,1 +1,9 @@
-const express = requier('express')
+const express = require('express')
+const router = express.Router()
+const CategoryController = require('../controllers/categoryController')
+
+router.get('/categories', CategoryController.readCategories)
+router.post('/categories', CategoryController.createCategories)
+router.delete('/categories/:id', CategoryController.deleteCategories)
+
+module.exports = router
--- server/user/routers/index.js
+++ server/user/routers/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const userRouter = require('./users')
-// const jobRouter = require('./jobs')
+const categoryRouter = require('./categories')
 // const companyRouter = require('./companies')
 // const { authentication } = require('../middlewares/authentication')
 
 router.use(userRouter)
-// router.use(jobRouter)
+router.use(categoryRouter)
 // router.use(authentication)
 // router.use(companyRouter)
 
