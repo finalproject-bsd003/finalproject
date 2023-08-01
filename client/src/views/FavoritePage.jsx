7f756cf5d76e0099900aac311887d9b7b3588565
MN3280 <martinishu93@gmail.com>
2023-08-01 13:44:18 +0700

    feat:favorite page added

---
+++ client/src/views/FavoritePage.jsx
@@ -0,0 +1,15 @@
+const FavoritePage = () => {
+  return (
+    <>
+      <h2
+        style={{
+          fontSize: "28px",
+          fontWeight: "bold",
+          fontFamily: "serif",
+        }}
+      ></h2>
+    </>
+  );
+};
+
+export default FavoritePage;
